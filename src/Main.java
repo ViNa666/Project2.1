6147156d9868a23e1ed53f16d5deca8954f0e63c
ViNa666 <legenda-2000@yandex.ru>
2022-01-08 21:45:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+        public static void main(String[] args) {
+                int ticketCost = 5555;
+                int mile = 20;
+                int bonusMiles = ticketCost/mile;
+
+                System.out.println("У вас " + bonusMiles + " бонусных миль");
+
+        }
+}
